430d3a60789d67c3f8c4f239449ead35f0655fc3
gOOvER <info@goover.de>
2025-12-28 15:58:04 +0100

    Add Donate button (top left)

--- src/App.vue
+++ src/App.vue
@@ -773,6 +773,12 @@ function showToast(message, type = 'success') {
   <div class="container">
     <header>
       <div class="header-top">
+        <a href="https://donate.goover.dev" target="_blank" rel="noopener noreferrer" class="btn-donate" title="Support this project">
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+          </svg>
+          <span>Donate</span>
+        </a>
         <button class="btn-lang" @click="toggleLanguage" :title="currentLang === 'en' ? 'Switch to German' : 'Zu Englisch wechseln'">
           <!-- Aktuelle Sprache -->
           <span class="current-lang">
